69e0b50470b7aecae90d1711cd7af8372cf0f27f
shyam <shyampawar22233@gmail.com>
2026-04-16 22:18:21 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ import {
   removePendingUpload,
 } from "./db";
 import "./App.css";
-
+  
 const DEFAULT_API_URL = import.meta.env.DEV
   ? `${window.location.protocol}//${window.location.hostname}:5000`
   : window.location.origin;
